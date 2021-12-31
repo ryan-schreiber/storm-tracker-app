ddf288c8a70fbff8916c6bba908da8666947bf48
Ryan Schreiber <ryanschreiber86@gmail.com>
2021-12-31 16:31:24 -0600

    boilerplate working code

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # storm-tracker-app
+
+### How to run
+
+Clone the repo...
+```bash
+git clone git@github.com:ryan-schreiber/storm-tracker-app.git
+```
+
+Start the docker services...
+```bash
+docker-compose up --build -d
+```
+
+### Where the services are running
+Frontend: [localhost:3000](localhost:3000)
+Backend: [localhost:3001](localhost:3001)
---
+++ api/main.py
@@ -0,0 +1,20 @@
+
+from flask import *
+app = Flask(__name__)
+
+@app.route('/')
+def health_check():
+    response = make_response(
+        jsonify(
+            {
+                "status": True, 
+            }
+        ),
+        200,
+    )
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
