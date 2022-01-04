1d771eb72ca762489eaa998d96371c636f935fc7
gregm94 <51940642+gregm94@users.noreply.github.com>
2022-01-03 20:58:12 -0600

    Hello World Test

--- api/main.py
+++ api/main.py
@@ -15,6 +15,18 @@ def health_check():
     response.headers["Content-Type"] = "application/json"
     return response
 
+@app.route('/hello')
+def hello_world():
+    response = make_response(
+        jsonify(
+            {
+                "hello": "world", 
+            }
+        ),
+        200,
+    )
+    response.headers["Content-Type"] = "application/json"
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
